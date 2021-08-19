1e4daaca35b2d9c8129af7a26a9315678acfbf9f
Vivek Guntuku <vivek.g@tvsnext.io>
2021-08-19 11:03:17 +0530

    [Validating Post Request for Creating and add item into playlist]

--- Spotify Validation/UnitTest1.cs
+++ Spotify Validation/UnitTest1.cs
@@ -13,9 +13,7 @@ namespace Spotify_Validation
         [TestMethod]
         public void ValidateGetUserId()
         {
-              string myToken = "Bearer BQDrDp5UpBBERvBVKpLC9-dhS-nELt4or9r6vC7j1nJD_Ejdc1gI1M55pF" +
-            "YaYu5zQAOSLPQRsIsdBrrgp_nv95kwryq4QVdhs3Yw4sTDS8qUf701NkvbQuTQpEGAmk061vgtk-2B8UvEDlOpT" +
-            "IFJlggVvK2UZxEQqy-tlwLCXxNCd-XoVKSDweO3abOARTTh_WYoD2MvePaoT9OoukRrhcm-R3x3mnjv-VQM1FCRIw";
+              string myToken = "Bearer BQCINCdXC61yeXlJzLluSEtgzagpBqU4-YiB_RquGTeMyqlqKtS1c7rJpskQDTcz-ZQjxn6lSj4LMznIJmrnf5WbGHitnv18tbNDGy1VueyUr9sb1ROJqIbAJcO6Vzor5TJYy8esRHtKVj4p5uD2C7BD3id3U7dV_ZdX7US53KnBlZyoPvIBj19r-efnJMyU62ufcFKG7n5IUva-iT9wKuuIkurGXYVSykLw4hiVAg";
 
             string getUrl = "https://api.spotify.com/v1/me";
             client = new RestClient();
@@ -26,7 +24,7 @@ namespace Spotify_Validation
 
             if(response.IsSuccessful)
             {
-                System.Console.WriteLine("test Validated successfully with status code "+response.StatusCode +" with response :"+response.Content);
+                System.Console.WriteLine("test Validated successfully with status code "+response.StatusCode +"\n with response :"+response.Content);
 
             }
             else
@@ -39,9 +37,7 @@ namespace Spotify_Validation
         [TestMethod]
         public void RetrunPlayListBasedOnUserId()
         {
-            string myToken = "Bearer BQBpy2U7dB6WJWPddDrJUxpnCouxwc_NhbUffzi4H_uK7l2-LzS3VuH_sPWF1ap" +
-                "71YtSiE-CarkYty-P6snRVgrUvi7P9y7fbTmH7pD2Szb4TMHx7JuS62SzhdfkWAPcjkDr3CQ1KazqWEHsNE" +
-                "bh8bnziUetj_CZhyb1shwHkhakEA6NrUJ6RfGz7RVRHbuMLtObHpBgezCX2ka1kpNL9qmNRL1bzpr1QEDdX-A6oA";
+            string myToken = "Bearer BQCINCdXC61yeXlJzLluSEtgzagpBqU4-YiB_RquGTeMyqlqKtS1c7rJpskQDTcz-ZQjxn6lSj4LMznIJmrnf5WbGHitnv18tbNDGy1VueyUr9sb1ROJqIbAJcO6Vzor5TJYy8esRHtKVj4p5uD2C7BD3id3U7dV_ZdX7US53KnBlZyoPvIBj19r-efnJMyU62ufcFKG7n5IUva-iT9wKuuIkurGXYVSykLw4hiVAg";
 
             string getUrl = "https://api.spotify.com/v1/me/playlists";
             client = new RestClient();
@@ -61,6 +57,59 @@ namespace Spotify_Validation
                 System.Console.WriteLine(response.ErrorMessage);
             }
 
+        }
+
+        [TestMethod]
+        public void AddItemToPlaylist()
+        {
+            string token = "Bearer BQDE4EotaEKC1mqbdMjjOteJUgL9X_N5YgZq8ZQuoyTziT4MEm2O_VlEwJ8JKt-XNKIUdZGP1Mk1GXVcHjjLkQHOKjjAawj1riW-OVWJZE5-HwxDnBoLH-P5GACuQaK6QHhjjKuDeOPtcKZZ2R6T7AczhRv2v5Hysol-2v8p_D7LVtRonQ93yftvQSYyRNOgv51ZIU83xZ_E37a_Moumc9RsTH7tMUo8RoJx7hQN4g";
+            string getUrl = "https://api.spotify.com/v1/playlists/2surb9o4zDYqsICbov4m8l/tracks?uris=spotify%3Atrack%3A5sbooPcNgIE22DwO0VNGUJ";
+            client = new RestClient();
+            request = new RestRequest(getUrl);
+            request.AddHeader("Authorization", "Token" + token);
+            response = client.Post(request);
+            Assert.AreEqual(201, (int)response.StatusCode);
+           // Assert.AreEqual(System.Net.HttpStatusCode.Created, response.StatusCode);
+
+            if (response.IsSuccessful)
+            {
+                System.Console.WriteLine("test Validated successfully with status code " + response.StatusCode + " with response :" + response.Content);
+
+            }
+            else
+            {
+                System.Console.WriteLine(response.ErrorException);
+                System.Console.WriteLine(response.ErrorMessage);
+            }
+        }
+        [TestMethod]
+        public void CreatePlaylist()
+        {
+            string myToken = "Bearer BQCLneQmnXVbY2RyvYOlED2YBF4P82dxz1aoUhXCcGALTlU0nPPYl5HeuCrr94fXAmI_MgBHEU2zlfXK9wPM_K2utYAftUfD5Vwm18RqXL44bimAz0dVAjSGwJghKaWSll_sQbLlYTCNBx2qWHTLEO1Wb2YTDViwObr8UcUCdL3UlcPYuytMlvDsAOfPb0W1VPVpfc4Z5GhsxKgDgLYV7qZSE-xf2vO-4UjTRCvfOQ";
+            string getUrl = "https://api.spotify.com/v1/users/lb61yakft7sa7njpc85qwkh8l/playlists";
+            string jsonBody = "{"+
+                                 "\"name\":\"Classics\","+
+                                 "\"description\": \"Recent songs\","+
+                                  "\"public\": false"+
+                               "}";
+            client = new RestClient();
+            request = new RestRequest(getUrl);
+            request.AddHeader("Authorization", "Token" + myToken);
+            request.AddJsonBody(jsonBody);
+            response = client.Post(request);
+            Assert.AreEqual(201, (int)response.StatusCode);
+            
+
+            if (response.IsSuccessful)
+            {
+                System.Console.WriteLine("test Validated successfully with status code " + response.StatusCode + " with response :" + response.Content);
+
+            }
+            else
+            {
+                System.Console.WriteLine(response.ErrorException);
+                System.Console.WriteLine(response.ErrorMessage);
+            }
         }
     }
 }
